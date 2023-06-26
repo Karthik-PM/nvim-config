a2eddaee683dc8aeb3b2412f5d9478b1c0895370
Karthik P M <72903849+Karthik-PM@users.noreply.github.com>
2023-06-26 20:55:38 +0530

    added configs
    this a new config file for nvim based on lua

---
+++ lua/primeengine/init.lua
@@ -0,0 +1,3 @@
+require("primeengine.remap")
+require("primeengine.set")
+
---
+++ lua/primeengine/packer.lua
@@ -0,0 +1,61 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+--
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use({ 
+        'rose-pine/neovim', as = 'rose-pine' ,
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+    use{
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('preservim/nerdtree')
+    use('lukas-reineke/indent-blankline.nvim')
+    use{
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+    }
+    use{
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+
+    }
+    use{
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+    }
+    --comment
+    use('tpope/vim-commentary');
+
+    use { 'neoclide/coc.nvim', branch='release' }
+    use 'lervag/vimtex'
+    use('nanozuki/tabby.nvim')
+    use('ryanoasis/vim-devicons')
+    use('hrsh7th/vim-vsnip')
+    use('hrsh7th/vim-vsnip-integ')
+    use { "ellisonleao/gruvbox.nvim" }
+end)
